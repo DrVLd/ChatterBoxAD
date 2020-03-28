cf6685479fd8f036f7b1a516dc82400a5df58320
Dmitry Savkin <dmitry.savkin.info@gmail.com>
2020-03-28 16:43:01 +0100

    Created class for user

---
+++ Core/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class User
+    {
+
+        public  long Id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public string Login
+        {
+            set
+            {
+                _login = value;
+            }
+            get
+            {
+                return _login;
+            }
+        }
+
+        public string Password
+        {
+
+            set
+            {
+                _password = value;
+
+            }
+            get
+            {
+                return _password; 
+            }
+        }
+
+        public string _password;
+
+        private long _id;
+
+        private string _login;
+    }
+}
