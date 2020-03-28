87ae75f06b6cd578bd2ffb63df405c248d250f7f
DrVLd <s0567075@htw-berlin.de>
2020-03-28 17:53:27 +0100

    Adding new classes

---
+++ Core/Message.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Message
+    {
+        public long MessageID{get;}
+        public string TextMessage { get; set;}
+        public User MsgReceiver { get; set;}
+        public User MsgSender { get; set;}
+        public Chat ChatField { get; set;}
+        public List <Attachment> Attachments { get; set;}
+        public DateTime MesssageDT { get;}
+
+        public Message(long messageID, string textMessage, User msgReceiver, User msgSender, Chat chatField, DateTime messsageDT)
+        {
+            MessageID = messageID;
+            TextMessage = textMessage ?? throw new ArgumentNullException(nameof(textMessage));
+            MsgReceiver = msgReceiver ?? throw new ArgumentNullException(nameof(msgReceiver));
+            MsgSender = msgSender ?? throw new ArgumentNullException(nameof(msgSender));
+            ChatField = chatField ?? throw new ArgumentNullException(nameof(chatField));
+            MesssageDT = messsageDT;
+        }
+
+        public void AddAttachment(Attachment attachment)
+        {
+            Attachments.Add(attachment);
+        }
+    }
+}
