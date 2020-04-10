fef7cc18f47ae27b70f5d65203949a54ac2e9287
Dmitry Savkin <dmitry.savkin.info@gmail.com>
2020-04-10 16:51:31 +0200

    added server listener

--- ChatterBoxAD/MainWindow.xaml.cs
+++ ChatterBoxAD/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,12 +26,41 @@ namespace ChatterBoxAD
         public MainWindow()
         {
             InitializeComponent();
+            threadReceive = new Thread(new ThreadStart(ReceivedByServer));
+            threadReceive.Start();
+        }
+        //============================================================Receive================================================================================
+        Thread threadReceive;
+        void ReceivedByServer()
+        {
+            Socket socketReceive = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            int portReceive = 40000;
+            IPEndPoint iPEndPointReceive = new IPEndPoint(IPAddress.Parse("127.0.0.1"), portReceive);
+            socketReceive.Bind(iPEndPointReceive);
+            socketReceive.Listen(10);
+            while (true)
+            {
+                Socket temp = null;
+                try
+                {
+                    temp = socketReceive.Accept();
+                    byte[] messageReceivedByServer = new byte[100];
+                    int sizeOfReceivedMessage = temp.Receive(messageReceivedByServer, SocketFlags.None);
+                    string str = Encoding.ASCII.GetString(messageReceivedByServer);
+                    MessagesLbl.Content =  "\r\nClient: " + str;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message + "\n" + ex.StackTrace + "\n" + ex.HelpLink + "\n" + ex.InnerException
+                            + "\n" + ex.Source + "\n" + ex.TargetSite);
+                }
+                finally
+                {
+                    temp.Close();
+                }
+            }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
     }
 
 }
